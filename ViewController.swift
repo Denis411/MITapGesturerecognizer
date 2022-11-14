12a2e0e4075a671a2a5e4a100daef499287a8406
Dennis <dnsljnv411@gmail.com>
2022-11-14 12:05:15 +0700

    First commit

---
+++ ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MockTapGestureRecognizer
+//
+//  Created by MTS on 14/11/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
