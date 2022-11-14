d80c1c9ac283e72e3bd4ce48808a00555b723ac4
Dennis <dnsljnv411@gmail.com>
2022-11-14 12:25:48 +0700

    Add buttons

--- ViewController.swift
+++ ViewController.swift
@@ -1,16 +1,51 @@
-//
-//  ViewController.swift
-//  MockTapGestureRecognizer
-//
-//  Created by MTS on 14/11/2565 BE.
-//
+// https://www.youtube.com/watch?v=lZHNi5PZr4E
 
 import UIKit
 
 class ViewController: UIViewController {
+    private let blueButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    private let redButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .red
+        button.layer.cornerRadius = 5
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addButtons()
+        view.backgroundColor = .gray
     }
 }
 
+private extension ViewController {
+    func addButtons() {
+        blueButton.frame = CGRect(x: 50,
+                            y: UIScreen.main.bounds.height - 150,
+                            width: 50,
+                            height: 50)
+        view.addSubview(blueButton)
+        blueButton.addTarget(self, action: #selector(handleBlueButtonTap), for: .touchUpInside)
+        
+        redButton.frame = CGRect(x: UIScreen.main.bounds.width - 100,
+                            y: UIScreen.main.bounds.height - 150,
+                            width: 50,
+                            height: 50)
+        view.addSubview(redButton)
+        redButton.addTarget(self, action: #selector(handleRedButtonTap), for: .touchUpInside)
+    }
+    
+    @objc func handleBlueButtonTap() {
+        
+    }
+    
+    @objc func handleRedButtonTap() {
+        
+    }
+}
